7f0f26af2c535dc70e9de5aef8f3a3bd1b7600de
lyh <lyhxiangyang@gmail.com>
2021-11-10 21:04:29 +0800

    bug

---
+++ datascript/测试数据/11月9日-E5-3KM/11月9日-E5-3KM/Process数据处理/2. 指定指标进行标准化.py
@@ -0,0 +1,98 @@
+# 需要标准化文件所在的路径
+import os.path
+from typing import List
+
+import pandas as pd
+
+# 需要标准化的错误码
+from utils.DataFrameOperation import SortLabels, PushLabelToFirst, PushLabelToEnd
+from utils.DataScripts import getDFmean, standard_file_time_coreDict, standard_file_time_core_faultyDict
+from utils.DefineData import TIME_COLUMN_NAME, FAULT_FLAG
+from utils.FileSaveRead import saveFaultyDict, readFilename_Time_Core_pdDict, readFilename_Time_Core_Faulty_pdDict, \
+    saveFilename_Time_Core_pdDict, saveFilename_Time_Core_Faulty_pdDict
+
+standardized_normalflag = 0
+standardized_abnormalflag = [15]
+# # 所有需要标准化的特征 这个变量没有被使用 仅供参考
+# allFeature = [
+#     # "time",
+#     # "pid",
+#     # "status",
+#     # "create_time",
+#     # "puids_real",
+#     # "puids_effective",
+#     # "puids_saved",
+#     # "pgids_real",
+#     # "pgids_effective",
+#     # "pgids_saved",
+#     "user",
+#     "system",
+#     # "children_user",
+#     # "children_system",
+#     "iowait",
+#     # "cpu_affinity",  # 依照这个来为数据进行分类
+#     "memory_percent",
+#     "rss",
+#     "vms",
+#     "shared",
+#     "text",
+#     "lib",
+#     "data",
+#     "dirty",
+#     "read_count",
+#     "write_count",
+#     "read_bytes",
+#     "write_bytes",
+#     "read_chars",
+#     "write_chars",
+#     "num_threads",
+#     "voluntary",
+#     "involuntary",
+#     # "faultFlag",
+# ]
+
+
+
+
+
+if __name__ == "__main__":
+    # 所有正常数据的路径
+    spath = "tmp/tData-11-09/测试数据/多机-E5-process-3KM"
+    normalpath = os.path.join(spath, "1.所有process错误码信息/0.csv")
+    # 需要标准化的数据路径
+    file_time_corePath = os.path.join(spath, "2.filename-time-core")
+    file_time_core_faultyPath = os.path.join(spath, "3.filename-time-core-faulty")
+    # 需要标准化的特征
+    standardfeatur = ["user", "system", "cpu"]
+    standardvalue = 60
+
+    # 获得所有正常情况下各个特征的平均值
+    nomalpd = pd.read_csv(normalpath)
+    normalmean = getDFmean(nomalpd, standardFeatures=standardfeatur)
+    # 这个平均值我一定要保存下来
+    print(normalmean)
+    normalmean.to_csv(os.path.join(spath, "标准化平均值.csv"))
+
+    # 读取所有的文件到字典中
+    print("开始读取file_time_core信息".center(40, "*"))
+    file_time_corePDDict = readFilename_Time_Core_pdDict(file_time_corePath)
+    print("结束读取file_time_core信息".center(40, "*"))
+    print("开始读取file_time_core_faulty信息".center(40, "*"))
+    file_time_core_faultyPDDict = readFilename_Time_Core_Faulty_pdDict(file_time_core_faultyPath)
+    print("结束读取file_time_core_faulty信息".center(40, "*"))
+
+    print("标准化开始处理".center(40, "*"))
+    file_time_corePDDict = standard_file_time_coreDict(file_time_corePDDict, standardFeature=standardfeatur,
+                                                       meanvalue=normalmean, standardValue=standardvalue)
+    file_time_core_faultyPDDict = standard_file_time_core_faultyDict(file_time_core_faultyPDDict,
+                                                                     standardFeature=standardfeatur,
+                                                                     meanvalue=normalmean, standardValue=standardvalue)
+    print("标准化处理结束".center(40, "*"))
+
+    # 将filename-time-core进行保存
+    sspath = os.path.join(spath, "4.filename-time-core-标准化")
+    saveFilename_Time_Core_pdDict(sspath, file_time_corePDDict)
+
+    # 将filename-time-core进行保存
+    sspath = os.path.join(spath, "5.filename-time-core-faulty-标准化")
+    saveFilename_Time_Core_Faulty_pdDict(sspath, file_time_core_faultyPDDict)
