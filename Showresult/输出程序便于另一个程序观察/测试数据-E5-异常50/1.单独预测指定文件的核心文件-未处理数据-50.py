71c14de3d56efed3b291e4511cfe1a02e447ee62
lyh <lyhxiangyang@gmail.com>
2021-11-11 16:09:27 +0800

    bug

---
+++ Showresult/输出程序便于另一个程序观察/测试数据-E5-异常50/1.单独预测指定文件的核心文件-未处理数据-50.py
@@ -0,0 +1,46 @@
+
+"""
+单独指定datascript中步骤3中生成的每个文件每个核心的文件使用现有模型进行预测，预测结果存储在
+"""
+import os.path
+
+import pandas as pd
+
+from Classifiers.TrainToTest import testThree
+
+# 将一个DataFrame的FAULT_FLAG重值为ff
+from utils.DataFrameOperation import mergeDataFrames
+from utils.DefineData import FAULT_FLAG
+
+
+def setPDfaultFlag(df: pd.DataFrame) -> pd.DataFrame:
+    realflag = list(df[FAULT_FLAG])
+    if FAULT_FLAG in df.columns.array:
+        df = df.drop(FAULT_FLAG, axis=1)
+    dealflag = [(i // 10) * 10 for i in realflag]
+    ffdict = {FAULT_FLAG: dealflag}
+    tpd = pd.DataFrame(data=ffdict)
+    tpd = pd.concat([df, tpd], axis=1)
+    return tpd
+
+if __name__ == "__main__":
+    spath = "tmp/allpreinformation"
+    # 使用模型路径
+    usemodelpath = "Classifiers/saved_model/tmp_load1_nosuffix"
+    # 要预测的文件
+    prefiles = [
+        "D:\HuaweiMachine\huaweicode-10-26\tmp\tData-11-09\测试数据\多机-E5-server-3KM\6.filename-time-标准化-特征提取-未处理首尾\wrf_e5_43_server\0.csv",
+    ]
+
+    # 要预测文件的路径
+    pdlist = []
+    for ipath in prefiles:
+        if not os.path.exists(ipath):
+            print("文件不存在")
+            exit(1)
+        prepd = pd.read_csv(ipath)
+        prepd = setPDfaultFlag(prepd)
+        pdlist.append(prepd)
+    prepd, err = mergeDataFrames(pdlist)
+    testThree(prepd, spath=spath, modelpath=usemodelpath)
+
