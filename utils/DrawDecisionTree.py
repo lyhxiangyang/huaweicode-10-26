8a0fcd24f25f8232cbfe9e84e1e57b74cd41b7b8
lyhxiangyang <lyhxiangyang@gmail.com>
2021-11-26 21:55:04 +0800

    bug

--- utils/DrawDecisionTree.py
+++ utils/DrawDecisionTree.py
@@ -8,12 +8,14 @@ import joblib
 from utils.DefineData import MODEL_TYPE
 
 
-def plot_tree_structure(depth, modelpath: str, filename: str = None):
+def plot_tree_structure(depth: int = None, modelpath: str = "", filename: str = None):
     if filename is None:
         filename = MODEL_TYPE[0] + ".pkl"
     # Load the model
     modelname = os.path.join(modelpath, filename)
     model = joblib.load(modelname)
+    if depth is None:
+        depth = model.max_depth
     # 读取标签信息
     with open("{}".format(os.path.join(modelpath, "alllabels.txt")), "r") as f:
         classes = f.read().splitlines()
