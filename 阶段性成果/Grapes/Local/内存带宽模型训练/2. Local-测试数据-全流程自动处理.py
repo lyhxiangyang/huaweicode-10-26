84a73f32eb9c1ae7f949984f8ee49a29641c6684
lyhxiangyang <lyhxiangyang@gmail.com>
2021-12-05 17:28:10 +0800

    bug

--- 阶段性成果/Grapes/Local/内存带宽模型训练/2. Local-测试数据-全流程自动处理.py
+++ 阶段性成果/Grapes/Local/内存带宽模型训练/2. Local-测试数据-全流程自动处理.py
@@ -1,5 +1,5 @@
 import os
-from typing import Set, Tuple
+from typing import Set, Tuple, List
 
 import pandas as pd
 
@@ -9,7 +9,19 @@ from utils.DefineData import TIME_COLUMN_NAME, PID_FEATURE, CPU_FEATURE, FAULT_F
 from utils.FileSaveRead import saveDFListToFiles
 from utils.auto_forecast import getfilespath, getfilepd, differenceProcess, add_cpu_column, differenceServer, \
     standardLists, changeTimeTo_pdlists, processpdsList, serverpdsList, deal_serverpds_and_processpds, \
-    predictAllAbnormal, analysePredictResult
+    predictAllAbnormal, analysePredictResult, removeAllHeadTail
+
+
+def getSelfMean(dfs: List[pd.DataFrame]) -> pd.Series:
+    allpds, _ = mergeDataFrames(dfs)
+    # 将所有的错误都掐头去尾
+    allfeatureMeanValue = removeAllHeadTail(allpds)
+    # 将所有的错误都分离开
+    fault_DataFrameDict = dict(list(allfeatureMeanValue.groupby(FAULT_FLAG)))
+    # 得到0的平均值
+    normalserver_meanvalue = fault_DataFrameDict[0][server_feature].mean()
+    return normalserver_meanvalue
+
 
 if __name__ == "__main__":
     # ============================================================================================= 输入数据定义
@@ -27,20 +39,60 @@ if __name__ == "__main__":
     # 预测内存泄露的模型路径
     servermemory_modelpath = R"tmp/modelpath/singlefeature/memory_leak_model"
     # 预测内存带宽的模型路径
-    serverbandwidth_modelpath = R"tmp/modelpath/singlefeature/memory_bandwidth_model"
+    serverbandwidth_modelpath = R"tmp/grapemodels/memory_bandwidth_model"
     # 将一些需要保存的临时信息进行保存路径
-    spath = "tmp/总过程分析/Grapes/测试数据-Local-1km"
+    spath = "tmp/总过程分析/Grapes/测试数据-Local"
     # 是否有存在faultFlag
     isExistFaultFlag = True
     # 核心数据 如果isManuallyspecifyCoreList==True那么就专门使用我手工指定的数据，如果==False，那么我使用的数据就是从process文件中推理出来的结果
-    coresnumber = 56  # 运行操作系统的实际核心数  如实填写
-    isManuallyspecifyCoreList = False # 默认自动生成
+    coresnumber = 104  # 运行操作系统的实际核心数  如实填写
+    isManuallyspecifyCoreList = False  # 默认自动生成
     wrfruncoresnumber = 104  # wrf实际运行在的核心数，如果isManuallyspecifyCoreList = False将会手工推导演
     coresSet = set(range(0, 103))  # wrf实际运行在的核心数
 
     # 需要对server数据进行处理的指标
-    server_feature = ["used", "pgfree"]
-    server_accumulate_feature = ["pgfree"]
+    server_feature = [
+        # "time",
+        "user",
+        "nice",
+        "system",
+        "idle",
+        "iowait",
+        "irq",
+        "softirq",
+        "steal",
+        "guest",
+        "guest_nice",
+        "ctx_switches",
+        "interrupts",
+        "soft_interrupts",
+        "syscalls",
+        "freq",
+        "load1",
+        "load5",
+        "load15",
+        "total",
+        "available",
+        "percent",
+        "used",
+        "free",
+        "active",
+        "inactive",
+        "buffers",
+        "cached",
+        "handlesNum",
+        "pgpgin",
+        "pgpgout",
+        "fault",
+        "majflt",
+        "pgscank",
+        "pgsteal",
+        "pgfree",
+        # "faultFlag",
+    ]
+    server_accumulate_feature = ['idle', 'iowait', 'interrupts', 'user', 'system', 'ctx_switches', 'soft_interrupts', 'irq',
+                  'softirq', 'steal', 'syscalls', 'handlesNum', 'pgpgin', 'pgpgout', 'fault', 'majflt', 'pgscank',
+                  'pgsteal', 'pgfree']
     # 需要对process数据进行处理的指标, cpu数据要在数据部分添加, 在后面，会往这个列表中添加一个cpu数据
     process_feature = ["user", "system"]
     process_accumulate_feature = ["user", "system"]
@@ -50,22 +102,22 @@ if __name__ == "__main__":
     process_time_format = '%Y/%m/%d %H:%M'
 
     # 预测是否使用阀值, True为使用阀值预测 必须指定thresholdValueDict， False代表使用模型进行预测, 必须设置好模型的路径
-    isThreshold = True
+    isThreshold = False  # 不使用阈值，使用模型
     thresholdValueDict = {
         "process_cpu_mean": 57,
         "used": 120,  # 不要改key值
-        "pgfree": 120
+        "pgfree": 130
     }
     # 是否使用正常文件中的平均值 True代表这个从正常文件中读取，False代表着直接从字典中读取
-    isFileMean = False # 不从文件中读取
+    isFileMean = False  # 不从文件中读取, 在这里不使用文件读取，就使用自己内部的平均值
     # 如果上面的是False，则使用下面的字典数据
     processmeanVaule = {
         "cpu": 60,
     }
-    # Local训练数据下面的平均值为下面
+    # 来自E5测试数据自身
     servermeanValue = {
-        "used": 27500000000,
-        "pgfree": 18000000,
+        "used": 27500000000,  # 56Bilion
+        "pgfree": 18000000,  # 这个pgfree是不准的
     }
 
     # ============================================================================================= 先将正常数据和预测数据的指标从磁盘中加载到内存中
@@ -137,7 +189,11 @@ if __name__ == "__main__":
         normalserver_meanvalue = getDFmean(allnormalserverpd, server_feature)
         normalprocess_meanvalue = getDFmean(allnormalprocesspd, process_feature)
     else:
-        normalserver_meanvalue = pd.Series(data=servermeanValue)
+        # 不从文件读取，那么server数据将需要从正常数据中读取
+        normalserver_meanvalue = getSelfMean(predictserverpds)
+        normalserver_meanvalue["used"] = servermeanValue["used"]
+        normalserver_meanvalue["pgfree"] = servermeanValue["pgfree"]
+        # 进程的平均值
         normalprocess_meanvalue = pd.Series(data=processmeanVaule)
     # 将这几个平均值进行保存
     tpath = os.path.join(spath, "1. 正常数据的平均值")
@@ -183,12 +239,11 @@ if __name__ == "__main__":
         spath=tpath,
         isThreshold=isThreshold,
         thresholdValue=thresholdValueDict,
-        modelfilepath=processcpu_modelpath
+        modelfilepath=processcpu_modelpath,
+        addserverfeatures=server_feature,
     )
     # ============================================================================================= 对使用到的核心数进行判断, 因为可能并不是全核心进行预测
     print("使用到的核心数进行判断".center(40, "*"))
-
-
     # 得到核心数和核心集合的函数
     def getcores(processpd: pd.DataFrame) -> Tuple[int, Set[int]]:
         coresSet = set(list(processpd[CPU_FEATURE]))
@@ -202,7 +257,8 @@ if __name__ == "__main__":
     print("wrf运行核心数量：{}".format(wrfruncoresnumber))
     print("核心的位数：{}".format(coresSet))
     with open(os.path.join(spath, "运行核心的数据.txt"), "w", encoding="utf-8") as f:
-        writeinfo = ["系统核心数量{}\n".format(coresnumber), "核心数量：{}\n".format(wrfruncoresnumber), "核心的位数：{}\n".format(coresSet)]
+        writeinfo = ["系统核心数量{}\n".format(coresnumber), "核心数量：{}\n".format(wrfruncoresnumber),
+                     "核心的位数：{}\n".format(coresSet)]
         f.writelines(writeinfo)
 
     # ============================================================================================= 对process数据和server数据合在一起进行预测
@@ -218,7 +274,8 @@ if __name__ == "__main__":
             thresholdValue=thresholdValueDict,
             Memory_bandwidth_modelpath=serverbandwidth_modelpath,
             Memory_leaks_modelpath=servermemory_modelpath,
-            coresnumber=wrfruncoresnumber
+            coresnumber=wrfruncoresnumber,
+            mem_bandwidth_features=server_feature,
         )
         # 对结果进行分析
         analysePredictResult(predictpd, tpath)
