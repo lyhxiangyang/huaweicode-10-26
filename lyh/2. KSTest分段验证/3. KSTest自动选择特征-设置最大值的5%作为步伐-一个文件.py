663fc35d2f98f2f882c29cb53dc59a5b354a2ecd
lyhxiangyang <lyhxiangyang@gmail.com>
2021-12-12 17:46:57 +0800

    bug

--- lyh/2. KSTest分段验证/3. KSTest自动选择特征-设置最大值的5%作为步伐-一个文件.py
+++ lyh/2. KSTest分段验证/3. KSTest自动选择特征-设置最大值的5%作为步伐-一个文件.py
@@ -12,24 +12,51 @@ from utils.FileSaveRead import saveDFListToFiles
 from utils.auto_forecast import getfilespath, getfilepd, differenceServer, removeAllHeadTail, smooth_dfs
 
 """
+得到一个异常前后的正常值
+# 确定
+"""
+def getNormalPD(abnormalserverPD: pd.DataFrame, abnormaltype: int):
+    flags = list(abnormalserverPD[FAULT_FLAG])
+    selectflag = [False] * len(flags)
+    faultbeginpos = -1
+    faultendpos = -1
+    for i, fault in enumerate(flags):
+        if fault == abnormaltype:
+            faultendpos = i
+        if fault == abnormaltype and faultbeginpos == -1:
+            faultbeginpos = i
+    # 将前面的正常标签找到
+    normalpos = faultbeginpos - 1
+    while normalpos >= 0:
+        if flags[normalpos] != 0:
+            break
+        selectflag[normalpos] = True
+        normalpos -= 1
+
+    normalpos = faultendpos + 1
+    while normalpos < len(flags):
+        if flags[normalpos] != 0:
+            break
+        selectflag[normalpos] = True
+        normalpos += 1
+    return abnormalserverPD[selectflag]
+
+"""
 比较两个server的DataFrame 异常为abnormal
 """
 
-
-def getServerDiffFeaturesFromTwoData(normalserverPD: pd.DataFrame, abnormalserverPD: pd.DataFrame, abnormaltype: int,
-                                     spath: str = ".") -> Dict[str, float]:
-    if not os.path.exists(spath):
-        os.makedirs(spath)
-    # 将所有的异常的首尾去掉
+def getServerDiffFeaturesFromOneData(abnormalserverPD: pd.DataFrame, abnormaltype: int, by_fdr: float = 0.05) -> Dict[str, float]:
     abnormalserverPD = removeAllHeadTail(abnormalserverPD, windowsize=3)
-    abnormal_pdDict = dict(list(abnormalserverPD.groupby(FAULT_FLAG)))  # 指定异常类型
-    specialAbnormalPd = abnormal_pdDict[abnormaltype]
+    abnormal_pdDict = dict(list(abnormalserverPD.groupby(FAULT_FLAG))) # 指定异常类型
+    specialAbnormalPd  = abnormal_pdDict[abnormaltype] # 异常类型
+    normalserverPD = abnormal_pdDict[0]
+    # normalserverPD = getNormalPD(abnormalserverPD=abnormalserverPD, abnormaltype=abnormaltype)
     print("正常数据长度：{}".format(len(normalserverPD)))
-    print("异常{}特征长度：{}".format(abnormaltype, len(specialAbnormalPd)))
+    print("异常特征{} 长度：{}".format(abnormaltype, len(specialAbnormalPd)))
 
-    # 得到各个特征对应的pvalue值
+    # =====================================================得到特征选择
     feature_pvalueDict = getPValueFromTwoDF(normalserverPD, specialAbnormalPd)
-    selectFeatureDict, notselectFeatureDict = getFeatureNameByBenjamini_Yekutiel(feature_pvalueDict, fdr=0.05)
+    selectFeatureDict, notselectFeatureDict = getFeatureNameByBenjamini_Yekutiel(feature_pvalueDict, fdr=by_fdr)
     selectFeatureList = sorted(selectFeatureDict.items(), key=lambda item: item[1], reverse=False)
     notselectFeatureList = sorted(notselectFeatureDict.items(), key=lambda item: item[1], reverse=False)
     print("特征总个数:{}".format(len(selectFeatureList) + len(notselectFeatureList)))
@@ -59,7 +86,6 @@ def cutOneDataframe(onepd: pd.DataFrame, serverfeature: List[str], feaMaxValue: 
         bins.append((maxvalues))
         labels = bins[0: -1]
         return pd.cut(x, bins=bins, labels=labels, right=False)
-
     onepd[serverfeature] = onepd[serverfeature].apply(oneColumnsFunction)
 
 
@@ -78,10 +104,6 @@ if __name__ == "__main__":
     predictdirpath = R"C:\Users\lWX1084330\Desktop\正常和异常数据\训练数据-E5-1km-异常数据"
     predictserverfiles = getfilespath(os.path.join(predictdirpath, "server"))
     predictprocessfiles = getfilespath(os.path.join(predictdirpath, "process"))
-
-    normaldirpath = R"C:\Users\lWX1084330\Desktop\正常和异常数据\E5-3km-正常数据"
-    normalserverfiles = getfilespath(os.path.join(normaldirpath, "server"))
-    normalprocessfiles = getfilespath(os.path.join(normaldirpath, "process"))
 
     abnormalType = 55  # 要比较的异常类型
     spath = "tmp/特征比较"  # 结果文件输出的路径
@@ -133,11 +155,11 @@ if __name__ == "__main__":
     # 每个特征值的取值返回，如果在这里，就按照这个范围进行使用，否则不在这个范围内的数值，会自动进行推导求出返回。
     # 格式为featurename: (lengthstep) lengthstep代表着多少长度以内视为同一个数字
     defaultPercentstep = 0.1  # 默认使用最大的值的0.05作为一个步
+    BY_FDR = 0.05
 
     # ============================================================================================= 先将正常数据和预测数据的指标从磁盘中加载到内存中
     print("将数据从文件中读取".center(40, "*"))
     predictserverpds = []
-    normalserverpds = []
     # 添加上时间和FAULT_FLAG
     time_server_feature = server_feature.copy()
     time_server_feature.extend([TIME_COLUMN_NAME, FAULT_FLAG])
@@ -145,22 +167,15 @@ if __name__ == "__main__":
         tpd = getfilepd(ifile)
         tpd = tpd.loc[:, time_server_feature]
         predictserverpds.append(tpd)
-    for ifile in normalserverfiles:
-        tpd = getfilepd(ifile)
-        tpd = tpd.loc[:, time_server_feature]
-        normalserverpds.append(tpd)
     # ============================================================================================= 对server文件中的累积指标进行差分
     print("将server数据中的累积指标进行差分处理".center(40, "*"))
-    difference_normal_serverpds = differenceServer(serverpds=normalserverpds,
-                                                   accumulateFeatures=server_accumulate_feature)
     difference_abnormal_serverpds = differenceServer(serverpds=predictserverpds,
                                                      accumulateFeatures=server_accumulate_feature)
     # ============================================================================================= 对server文件中进行平滑处理
     print("将server数据中的指标进行平滑处理".center(40, "*"))
     smooth_dfs(difference_abnormal_serverpds)
-    smooth_dfs(difference_normal_serverpds)
     # ============================================================================================= 对server文件中最大值和最小值的
-    allserverpd, _ = mergeDataFrames(difference_abnormal_serverpds + difference_normal_serverpds)
+    allserverpd, _ = mergeDataFrames(difference_abnormal_serverpds)
     serverminValue = allserverpd[server_feature].min()
     servermaxValue = allserverpd[server_feature].max()
 
@@ -169,17 +184,12 @@ if __name__ == "__main__":
     servermaxValue = servermaxValue + 1
 
     # ============================================================================================= 将每一列的最大值的5% 或者 其他数值作为一步
-    cutDataframeLists(difference_normal_serverpds, serverfeature=server_feature, feaMaxValue=servermaxValue,
-                      percentstep=defaultPercentstep)
     cutDataframeLists(difference_abnormal_serverpds, serverfeature=server_feature, feaMaxValue=servermaxValue,
                       percentstep=defaultPercentstep)
-    saveDFListToFiles(os.path.join(spath, "正常server"), difference_normal_serverpds)
     saveDFListToFiles(os.path.join(spath, "异常server"), difference_abnormal_serverpds)
     # ============================================================================================= KSTest进行比较
-    allnormalpds, _ = mergeDataFrames(difference_normal_serverpds)
     allabnormalpds, _ = mergeDataFrames(difference_abnormal_serverpds)
-    # 将文件保存
-    getServerDiffFeaturesFromTwoData(normalserverPD=allnormalpds, abnormalserverPD=allabnormalpds, abnormaltype=55)
+    getServerDiffFeaturesFromOneData(abnormalserverPD=allabnormalpds, abnormaltype=abnormalType, by_fdr=BY_FDR)
 
 
 
