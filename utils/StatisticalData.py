9f4c67d34eb52b2ee277f30a8f5d3c29b16bf9d8
lyhxiangyang <lyhxiangyang@gmail.com>
2021-12-14 00:04:35 +0800

    bug

--- utils/StatisticalData.py
+++ utils/StatisticalData.py
@@ -1,6 +1,8 @@
 """
 和统计有关系的文件
 """
+from typing import List
+
 import pandas as pd
 
 from utils.auto_forecast import getDetailedInformationOnTime
@@ -19,11 +21,17 @@ def removeAbnormalPoints(df: pd.DataFrame, testLabelName: str = "preFlag") -> pd
         if x.count(nowlable) == x.count(maxlabel):
             return nowlable
         return maxlabel
-    df[testLabelName].rolling(window=winsize, center=True, min_periods=1).agg([getRightLabel])
+    df[testLabelName] = df[testLabelName].rolling(window=winsize, center=True, min_periods=1).agg([getRightLabel])
     return df
 
-def getTimePeriod(df: pd.DataFrame, timename: str = "time", realLabelName: str = "faultFlag", testLabelName: str = "preFlag") -> pd.DataFrame:
-    tpd = getDetailedInformationOnTime(df, timeLabelName=timename, realFlagName=realLabelName, testFlagName=testLabelName)
+def makeProbabilityToFloat(probability: List[str]) -> List[float]:
+    def trans(s: str)->float:
+        return float(s[0:-1]) / 100
+    return [trans(i) for i in probability]
+
+
+def getTimePeriod(df: pd.DataFrame, timename: str = "time", realLabelName: str = "faultFlag", testLabelName: str = "preFlag", probabilityName="概率") -> pd.DataFrame:
+    tpd = getDetailedInformationOnTime(df, timeLabelName=timename, realFlagName=realLabelName, testFlagName=testLabelName, probabilityName=probabilityName)
     feas = ["检测开始时间", "实际开始时间", "检测结束时间", "实际结束时间", "检测标记", "实际标记", "概率", "检测运行时间","实际运行时间","重叠时间"]
     return tpd[feas]
 
