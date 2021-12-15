154da940bc55f41c14019f2a667611b7024099c5
lyhxiangyang <lyhxiangyang@gmail.com>
2021-12-15 10:48:20 +0800

    bug

--- lyh/3. 得到准确率/1.得到目标文件准确率.py
+++ lyh/3. 得到准确率/1.得到目标文件准确率.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
 from utils.DataFrameOperation import mergeDataFrames
-from utils.auto_forecast import removeAllHeadTail, getAccuracy
+from utils.DefineData import FAULT_FLAG
+from utils.auto_forecast import removeAllHeadTail, getAccuracy, remove_Abnormal_Head_Tail
 
 if __name__ == "__main__":
     accuracyFilesPath = [
         R"tmp/总过程分析/测试数据-E5-1KM/6. 最终预测结果/预测结果.csv",
         R"tmp/总过程分析/测试数据-E5-3KM/6. 最终预测结果/预测结果.csv",
-        R"tmp/总过程分析/测试数据-E5-8KM/6. 最终预测结果/预测结果.csv",
+        R"tmp/总过程分析/测试数据-E5-9KM/6. 最终预测结果/预测结果.csv",
         R"tmp/总过程分析/测试数据-E5-RST/6. 最终预测结果/预测结果.csv",
         R"tmp/总过程分析/测试数据-Local-1km/6. 最终预测结果/预测结果.csv",
         R"tmp/总过程分析/测试数据-Local-3km/6. 最终预测结果/预测结果.csv",
@@ -15,7 +16,9 @@ if __name__ == "__main__":
     ]
     accpds = [pd.read_csv(ipath) for ipath in accuracyFilesPath]
     # 去除每个点的首尾连个字符
+    accpds = [remove_Abnormal_Head_Tail(predictpd, windowsize=3, abnormals={41, 42, 43, 44, 45, 71, 72, 73, 74, 75, 91, 92, 93, 94, 95}) for predictpd in accpds]
     accpds = [ removeAllHeadTail(ipd, windowsize=3) for ipd in accpds ]
+    p = [ getAccuracy(list(ipd[FAULT_FLAG]), list(ipd["preFlag"]), excludeflags=[0]) for ipd in accpds]
     # 合并
     allaccpd, _ = mergeDataFrames(accpds)
     faultFlagName = list(allaccpd["faultFlag"])
@@ -23,19 +26,19 @@ if __name__ == "__main__":
     acc = getAccuracy(realflags=faultFlagName, preflags=preFlagName, excludeflags=[0])
     print("wrf所有的准确率：{:.2%}".format(acc))
 
-    accuracyFilesPath = [
-        R"tmp/总过程分析/Grapes/测试数据-E5/6. 最终预测结果/预测结果.csv",
-        R"tmp/总过程分析/Grapes/测试数据-Local/6. 最终预测结果/预测结果.csv",
-    ]
-    accpds = [pd.read_csv(ipath) for ipath in accuracyFilesPath]
-    # 去除每个点的首尾连个字符
-    accpds = [removeAllHeadTail(ipd, windowsize=3) for ipd in accpds]
-    # 合并
-    allaccpd, _ = mergeDataFrames(accpds)
-    faultFlagName = list(allaccpd["faultFlag"])
-    preFlagName = list(allaccpd["preFlag"])
-    acc = getAccuracy(realflags=faultFlagName, preflags=preFlagName)
-    print("Grape所有的准确率：{:.2%}".format(acc))
+    # accuracyFilesPath = [
+    #     R"tmp/总过程分析/Grapes/测试数据-E5/6. 最终预测结果/预测结果.csv",
+    #     R"tmp/总过程分析/Grapes/测试数据-Local/6. 最终预测结果/预测结果.csv",
+    # ]
+    # accpds = [pd.read_csv(ipath) for ipath in accuracyFilesPath]
+    # # 去除每个点的首尾连个字符
+    # accpds = [removeAllHeadTail(ipd, windowsize=3) for ipd in accpds]
+    # # 合并
+    # allaccpd, _ = mergeDataFrames(accpds)
+    # faultFlagName = list(allaccpd["faultFlag"])
+    # preFlagName = list(allaccpd["preFlag"])
+    # acc = getAccuracy(realflags=faultFlagName, preflags=preFlagName)
+    # print("Grape所有的准确率：{:.2%}".format(acc))
 
 
 
