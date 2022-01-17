812cb9302cb655daa6e62369571cdbec7a3e1217
lyhxiangyang <lyhxiangyang@gmail.com>
2022-01-17 10:13:18 +0800

    bug

--- Showresult/DrawTree/1. 画出决策树.py
+++ Showresult/DrawTree/1. 画出决策树.py
@@ -2,6 +2,4 @@ from utils.DrawDecisionTree import plot_tree_structure
 
 if __name__ == "__main__":
     # plot_tree_structure(modelpath="Classifiers/saved_model/tmp_load1_nosuffix")
-    plot_tree_structure(modelpath="tmp/modelpath/singlefeature/memory_leak_model")
-    plot_tree_structure(modelpath="tmp/modelpath/singlefeature/memory_bandwidth_model")
-    plot_tree_structure(modelpath="tmp/modelpath/singlefeature/process_cpu_model")
+    plot_tree_structure(modelpath="tmp/modelpath/l2/温度过高")
--- Showresult/DrawTree/2. 修改决策树.py
+++ Showresult/DrawTree/2. 修改决策树.py
@@ -1,6 +1,6 @@
 from Classifiers.TrainToTest import show_threshold, change_threshold
 
 if __name__ == '__main__':
-    file = r'tmp/modelpath/singlefeature/memory_bandwidth_model/decision_tree.pkl'
+    file = r'tmp/modelpath/l2/温度过高/decision_tree.pkl'
     show_threshold(file)
     change_threshold(file, 0, 500)
