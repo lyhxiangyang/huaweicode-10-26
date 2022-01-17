a42cf296880625b6ab8e8b68f62efeb11009f4b2
lyhxiangyang <lyhxiangyang@gmail.com>
2022-01-17 11:25:41 +0800

    bug

---
+++ L2层和L3层结合预测数据/4. 使用输入输出接口对测试数据进行检测/runScripts.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+import sys
+import time
+
+"""
+脚本作用自动运行1-19中的脚本
+"""
+runSeries = list(range(1, 20))
+if __name__ == "__main__":
+    runscriptpath = os.path.join(sys.path[0]) # 得到当前脚本的相对路径
+    dirs = os.listdir(runscriptpath)
+    for idir in dirs:
+        print("处理-{}".format(idir), end=" ")
+        dirnumber = int(idir.split(".")[0])
+        if dirnumber not in runSeries:
+            print("跳过")
+            continue
+        scriptorder1 = "python3 " + os.path.join(runscriptpath, idir, "1.generateJson.py")
+        p = subprocess.Popen(
+            scriptorder1,
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        p.communicate()
+        time.sleep(0.01)
+        stat = p.poll()
+        if stat != 0:
+            print("运行异常")
+            break
+        print("运行成功")
+
+
+
+
+
--- Showresult/DrawTree/2. 修改决策树.py
+++ Showresult/DrawTree/2. 修改决策树.py
@@ -3,4 +3,4 @@ from Classifiers.TrainToTest import show_threshold, change_threshold
 if __name__ == '__main__':
     file = r'tmp/modelpath/l2/温度过高/decision_tree.pkl'
     show_threshold(file)
-    change_threshold(file, 0, 500)
+    change_threshold(file, 0, 150.1)
