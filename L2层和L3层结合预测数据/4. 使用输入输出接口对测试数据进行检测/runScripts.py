c7b1ea9676be7a22b7e87b40f4a88d720cdea051
lyhxiangyang <lyhxiangyang@gmail.com>
2022-01-17 11:50:22 +0800

    bug

--- L2层和L3层结合预测数据/4. 使用输入输出接口对测试数据进行检测/runScripts.py
+++ L2层和L3层结合预测数据/4. 使用输入输出接口对测试数据进行检测/runScripts.py
@@ -7,25 +7,34 @@ import time
 脚本作用自动运行1-19中的脚本
 """
 runSeries = list(range(1, 20))
+result = []
 if __name__ == "__main__":
     runscriptpath = os.path.join(sys.path[0]) # 得到当前脚本的相对路径
     dirs = os.listdir(runscriptpath)
     for idir in dirs:
-        print("处理-{}".format(idir), end=" ")
         dirnumber = int(idir.split(".")[0])
         if dirnumber not in runSeries:
-            print("跳过")
+            result.append("处理-{} 跳过".format(idir))
             continue
+
+        print("处理{}中".format(idir).center(50, "#"))
 
         scriptorder1 = "python3 " + os.path.join(runscriptpath, idir, "1.generateJson.py")
         with subprocess.Popen(scriptorder1, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
-            p.communicate()
-            time.sleep(0.01)
             stat = p.poll()
-            if stat != 0:
-                print("运行异常")
+            if stat is None:
+                line = p.stdout.readline().strip()
+                print(line)
+            elif stat == 0:
+                result.append("处理-{} 成功运行".format(idir))
+                line = p.stderr.readline().strip()
+                print(line)
                 break
-            print("运行成功")
+            else:
+                result.append("处理-{} 异常退出".format(idir))
+
+    # 输出每个文件的最终结果
+    print(result)
 
 
 
