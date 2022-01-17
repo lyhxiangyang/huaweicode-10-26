ec24b7c6cf986d19f0343edc8791e780f198250e
lyhxiangyang <lyhxiangyang@gmail.com>
2022-01-18 05:51:26 +0800

    bug

---
+++ L2层和L3层结合预测数据/4. 使用输入输出接口对测试数据进行检测/deleteResult.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+"""
+脚本作用自动运行1-19中的脚本
+"""
+runSeries = list(range(1, 20))
+if __name__ == "__main__":
+    runscriptpath = os.path.join(sys.path[0])  # 得到当前脚本的相对路径
+    dirs = os.listdir(runscriptpath)
+    for idir in dirs:
+        # 不是目录就continue
+        if not os.path.isdir(os.path.join(runscriptpath, idir)):
+            continue
+        dirnumber = int(idir.split(".")[0])
+        if dirnumber not in runSeries:
+            continue
+        nowpath = os.path.join(runscriptpath, idir)
+        deletepath = os.path.join(nowpath, "centos11", "jsonfile")
+        if os.path.exists(deletepath):
+            shutil.rmtree(deletepath)
+
--- Showresult/DrawTree/2. 修改决策树.py
+++ Showresult/DrawTree/2. 修改决策树.py
@@ -1,6 +1,17 @@
 from Classifiers.TrainToTest import show_threshold, change_threshold
 
 if __name__ == '__main__':
+    # 温度过高由于模型训练的是相当于低强度，我们这里是高强度  所以修改原始数据集
     file = r'tmp/modelpath/l2/温度过高/decision_tree.pkl'
     show_threshold(file)
     change_threshold(file, 0, 150.1)
+
+    # 修改pfs网络异常，原始是121  改为200
+    file = r'tmp/modelpath/l2/网络异常1/decision_tree.pkl'
+    show_threshold(file)
+    change_threshold(file, 0, 200)
+
+    # 机器功率封顶原本设置为98.5 根据指标是cpu_pwer 但是现在要设置为70
+    file = r'tmp/modelpath/l2/机器功率封顶/decision_tree.pkl'
+    show_threshold(file)
+    change_threshold(file, 0, 70)
