a74b8be317188e9f34f318af4e1a5359a7d4f547
lyhxiangyang <lyhxiangyang@gmail.com>
2022-01-24 14:41:40 +0800

    bug

--- lyh/4. 分析topdown数据/修改数据.py
+++ lyh/4. 分析topdown数据/修改数据.py
@@ -32,7 +32,15 @@ def dealOneTopDownPD(topdownpd: pd.DataFrame) -> pd.DataFrame:
     # mflops平均值
     cname = "mflops_sliding"
     mflops_mean = topdownpd[cname][0:3].mean()
-    print(mflops_mean)
+    print("mflops平均值：{}".format(mflops_mean))
+    mflops_change = topdownpd[cname].apply(lambda x : (mflops_mean - x) / mflops_mean if x <= mflops_mean else 1 ) # 如果是-20% 那么对应的值应该增加20%
+
+    # 对ddrc_rd进行分析
+    cname = "ddrc_rd_sliding"
+    ddrc_rd_mean = topdownpd[cname][0:3].mean() # 得到一个正常值
+    print("{}平均值：{}".format(cname, ddrc_rd_mean))
+    topdownpd[cname+"_recover"] = topdownpd[cname] + ddrc_rd_mean * mflops_change
+
 
     return topdownpd
 
