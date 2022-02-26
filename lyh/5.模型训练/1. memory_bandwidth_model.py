449a5e6e7f7bf23761957630d7725bb2b77b1269
lyhxiangyang <lyhxiangyang@gmail.com>
2022-02-26 08:28:47 +0800

    bug

--- lyh/5.模型训练/1. memory_bandwidth_model.py
+++ lyh/5.模型训练/1. memory_bandwidth_model.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # 需要用到的三个路径
     nowpath = sys.path[0]
     savemodelpath = os.path.join(nowpath, "models", "grapes")
-    traindatapath = os.path.join(nowpath, "model_grape_50.csv")
+    traindatapath = os.path.join(nowpath, "model_grape_50_110.csv")
     # trainFeatures = ["ddrc_ddwr_sum"]
     trainFeatures = ["pgfree_mean",]
 
