fb04f7589c1a96a4f34afe02b3f06d419d7c65f0
lyhxiangyang <lyhxiangyang@gmail.com>
2022-04-07 20:38:56 +0800

    bug

--- lyh/8. 对测试数据进行分析/2.运行所有的目录文件.py
+++ lyh/8. 对测试数据进行分析/2.运行所有的目录文件.py
@@ -7,20 +7,24 @@ from hpc.l3l2utils.L2L3Main import detectionFromInputDict
 from hpc.l3l2utils.ParsingJson import readJsonToDict
 
 alldatadirs = [
-        R"DATA/测试数据/WRF/1KM",
-        R"DATA/测试数据/WRF/3KM",
-        R"DATA/测试数据/WRF/9KM",
-        R"DATA/测试数据/Grapes/test1",
-        R"DATA/测试数据/Grapes/国家超算",
+    R"DATA/测试数据/WRF/1KM",
+    R"DATA/测试数据/WRF/3KM",
+    R"DATA/测试数据/WRF/9KM",
+    R"DATA/测试数据/Grapes/test1",
+    R"DATA/测试数据/Grapes/国家超算",
 ]
+
 
 def getDirs(dirpaths) -> List[str]:
     dirnamess = os.listdir(dirpaths)
-    dirlists = [os.path.join(dirpaths, idir, "centos11") for idir in dirnamess if os.path.exists(os.path.join(dirpaths, idir, "centos11"))]
-    dirlists.extend([os.path.join(dirpaths, idir, "centos16") for idir in dirnamess if os.path.exists(os.path.join(dirpaths, idir, "centos16"))])
+    dirlists = [os.path.join(dirpaths, idir, "centos11") for idir in dirnamess if
+                os.path.exists(os.path.join(dirpaths, idir, "centos11"))]
+    dirlists.extend([os.path.join(dirpaths, idir, "centos16") for idir in dirnamess if
+                     os.path.exists(os.path.join(dirpaths, idir, "centos16"))])
     # dirlists.extend([os.path.join(dirpaths, idir, "centos21") for idir in dirnamess if os.path.exists(os.path.join(dirpaths, idir, "centos21"))])
     # dirlists.extend([os.path.join(dirpaths, idir, "centos26") for idir in dirnamess if os.path.exists(os.path.join(dirpaths, idir, "centos26"))])
     return dirlists
+
 
 # 指定某一个文件夹进行运行
 substrrundir = None
@@ -34,7 +38,7 @@ if __name__ == "__main__":
     for i in alldatadirs:
         alldatapath.extend(getDirs(i))
     if substrrundir is not None:
-       alldatapath = [i for i in alldatapath if substrrundir in i]
+        alldatapath = [i for i in alldatapath if substrrundir in i]
     for ipath in alldatapath:
         startTime1 = time.perf_counter()
         configJsonDict["predictdirjsonpath"] = os.path.join(ipath, "jsonfile", "alljson.json")
